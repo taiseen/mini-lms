e1f5751f07324ea1ca3e4ec177651934509a423c
taiseen <taiseen.cse@gmail.com>
2023-11-26 17:08:31 +0600

    chore: code style formating

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Learning Note:
 `page.tsx`    ==> hold `content` / ui text
 `layout.tsx`  ==> hold `structure` / component position / style
 
-`page.tsx`	  ==> return --> JSX
+`page.tsx`    ==> return --> JSX
 `layout.tsx`  ==> wrap   --> {children} & can hold other components
 
 * (group route) ==> can style the whole group using layout
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,10 +1,10 @@
-import { LayoutType } from "@/types";
-import Sidebar from "./_components/Sidebar";
 import TopNavbar from "./_components/TopNavbar";
+import Sidebar from "./_components/Sidebar";
+import { LayoutType } from "@/types";
 
 const DashboardLayout = ({ children }: LayoutType) => {
   return (
-    <main className="bg-red-200 h-screen">
+    <main className="">
       <nav className="fixed w-full h-20 md:pl-60 inset-y-0 z-50">
         <TopNavbar />
       </nav>
@@ -13,7 +13,7 @@ const DashboardLayout = ({ children }: LayoutType) => {
         <Sidebar />
       </aside>
 
-      <div className="md:ml-60 mt-20">{children}</div>
+      <div className="md:ml-60 mt-20 bg-red-200">{children}</div>
     </main>
   );
 };
